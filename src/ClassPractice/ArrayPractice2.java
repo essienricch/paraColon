5152b0674ac628a60dcd3e4c6b07f4a621e32390
essienricch <essienrichard98@gmail.com>
2022-09-21 12:11:01 -0700

    new

---
+++ src/ClassPractice/ArrayPractice2.java
@@ -0,0 +1,12 @@
+package ClassPractice;
+
+public class ArrayPract2 {
+    public static void main(String[] args) {
+        String [] color = new String[7];
+         color = new String[]{"Blue", "Red", "Black", "Purple", "Grey", "Ice", "White"};
+        for (int index = 0; index <= color.length; index++){
+            System.out.println(color[index]);
+
+        }
+    }
+}
---
+++ src/ClassPractice/MethodOverloading.java
@@ -0,0 +1,2 @@
+package ClassPractice;public class MethodOverloading {
+}
